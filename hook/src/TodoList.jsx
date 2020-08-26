95c1247e45ef1951dfe4136b21d2ea03f39a21ef
zhengyang.shi <zhengyang.shi@ipinyou.com>
2020-08-26 23:39:04 +0800

    todolist

--- hook/src/TodoList.jsx
+++ hook/src/TodoList.jsx
@@ -1,42 +1,94 @@
-import React, { useCallback, useState, useRef } from 'react';
+import React, { useCallback, useState, useRef, useEffect, createContext, useContext, useMemo, memo } from 'react';
 import './App.css'
 let idSeq = Date.now();
 
-function Control(props) {
-  const { addTodo } = props; 
-  const inputRef = useRef();
-  const onSubmit = (e) => {
-    e.preventDefault();
-    const newText = inputRef.current.value.trim(); // trim() 清除value的空白
-    if (newText.length === 0) {
-      return;
-    } 
-    addTodo({
-      id: ++idSeq,
-      text: newText,
-      complete: false,
-    });
-    inputRef.current.value = ''
-    onSubmit()
-  }
-  return (
-    <div className="control">
-      <h1>TODOS</h1>
-      <form action="" onSubmit={onSubmit}>
-        <input type="text" className='new-todo' placeholder="请输入内容" ref={inputRef}/>
-      </form>
-    </div>
-  )
-}
+const CountContext = createContext()
 
-function Todos() {
-  return (
-    <>
-    </>
-  )
-}
+const Control = memo(
+  function Control(props) {
+    const { addTodo } = props;
+    const name = useContext(CountContext)
+    const inputRef = useRef();
+    const onSubmit = (e) => {
+      e.preventDefault();
+      const newText = inputRef.current.value.trim(); // trim() 清除value的空白
+      if (newText.length === 0) {
+        return;
+      } 
+      addTodo({
+        id: ++idSeq,
+        text: newText,
+        complete: false,
+      });
+      inputRef.current.value = ''
+    }
+    return (
+      <div className="control">
+        <h1>TODOS</h1> 
+        <span>{name}</span>
+        <form action="" onSubmit={onSubmit}>
+          <input type="text" className='new-todo' placeholder="请输入内容" ref={inputRef}/>
+        </form>
+      </div>
+    )
+  }
+) 
+  
+const TodoItem = 
+  function TodoItem(props) {
+    const {
+      todo: {
+        id,
+        text,
+        complete
+      },
+      toggleTodo,
+      removeTodo
+    } = props
+    const onChange = () => {
+      toggleTodo(id);
+    };
+    const onRemove = () => {
+      removeTodo(id);
+    };
+    return (
+      <li className="todo-item">
+        <input type="checkbox" onChange={onChange} checked={complete}/>
+        <label className={complete ? 'complete': null}>{text}</label>
+        <button onClick={onRemove}>x</button>
+      </li>
+    )
+  }
+
+const Todos = memo(
+  function Todos(props) {
+    const { todos, toggleTodo, removeTodo, onClick } = props;
+    console.log(onClick);
+    return (
+      <>
+      <ul>
+        { 
+          todos.map( todo => {
+            return (<TodoItem 
+              key={todo.id}
+              todo={todo}
+              toggleTodo={toggleTodo}
+              removeTodo={removeTodo}
+            />)
+          })
+        }
+      </ul>
+      </>
+    )
+  }
+)
+  
+
+const LS_KEY = '_$-todos_';
 function TodoList () {
   const [todos, setTodos] = useState([]); // 这么写给函数赋予了类组件的能力，有数据了
+  const [name] = useState("石小阳")
+  const [count, setCount] = useState(0)
   const addTodo = (todo) => {
     setTodos(() => [...todos, todo]); 
   };
@@ -58,10 +110,26 @@ function TodoList () {
           : todo
       }))
     }, [])
+
+    useEffect(() => {
+      const todos = JSON.parse(localStorage.getItem(LS_KEY))
+      setTodos(todos);
+    }, [])
+
+    useEffect(() => {
+      localStorage.setItem(LS_KEY,  JSON.stringify(todos));  
+    }, [todos]);
+
+    const onClick = () => {
+      console.log('Click')
+    }
   return(
     <div className="todo-list">
-      <Control addTodo={addTodo} />
-      <Todos removeTodo={removeTodo} toggleTodo={toggleTodo}/>
+      <CountContext.Provider value={name}>
+        <button type="button" onClick={() => {setCount(count + 1)}}>点赞</button>
+        <Control addTodo={addTodo} />
+      </CountContext.Provider>
+      <Todos todos={todos} onClick={onClick} removeTodo={removeTodo} toggleTodo={toggleTodo}/>
     </div>
   )
 }
